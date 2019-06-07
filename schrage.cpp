177cf234500895940e91ef22698d2aa0020f3033
urbwacha <48128976+urbwacha@users.noreply.github.com>
2019-06-07 12:18:15 +0200

    Add files via upload

---
+++ schrage.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <vector>
+#include <conio.h>
+
+
+using namespace std;
+
+/// klasa do algorytmu rpq
+class RPQ {
+public:
+	int r;      ///czas przygotowania
+	int p;      ///czas wykonania
+	int q;      ///czas dostarczen
+	int nr;     ///iloscZadan
+	RPQ(int r, int p, int q, int nr) : r(r), p(p), q(q), nr(nr) {}
+	RPQ() { p = r = q = nr = 0; }
+	friend ostream & operator << (ostream & os, const RPQ & r);
+};
+
+ostream & operator << (ostream & os, const RPQ & r)
+{
+	os << r.nr + 1 << "  ";
+	return os;
+}
+
+///struktury dla dwoch czasow - przygotowania i dostarczania
+struct R { bool operator ()(const RPQ & a, const RPQ & b) { return a.r > b.r; } };
+struct Q { bool operator ()(const RPQ & a, const RPQ & b) { return a.q < b.q; } };
+
+
+/// funkcja max
+int max(int a, int b)
+{
+	if (a < b) return b;
+	else
+		return a;
+}
+
+
+int main()
+{
+	priority_queue<RPQ, vector<RPQ>, Q> wektorG;    ///zbior zadan gotowych do uszeregowania - kolejka priorytetowa
+	priority_queue<RPQ, vector<RPQ>, R> wektorN;    ///zbior zadan nieuszeregowanych - kolejka priorytetowa
+
+	ifstream file;              ///wczytywanie pliku
+	file.open("in200.txt");
+	int liczbaDanych = 0;
+	int r, p, q;
+	int iloscZadan = 0;
+	int t = 0, k = 0, Cmax = 0;
+	file >> liczbaDanych;
+	file >> r;
+
+/** wyswietlanie danych z pliku **/
+	for (int i = 0; i < liczbaDanych; ++i)
+	{
+		file >> r >> p >> q;
+		///cout << r << "   " << p << "   " << q << endl;
+		RPQ Dane(r, p, q, iloscZadan);
+		wektorN.push(Dane);
+		iloscZadan++;
+	}
+
+///	cout << "\n\n";
+	cout << "Kolejnosc wykonywanych zadan : \n";
+
+
+	while (wektorG.empty() == false || wektorN.empty() == false) { ///dopoki wektor G lub wektor N nie sa puste
+		///budowanie zbioru zadan gotowych do uszeregwania
+		while (wektorN.empty() == false && wektorN.top().r <= t) { ///dopoki wektor N nie jest pusty oraz czas przygotowania pierwszego zadania jest mniejszy badz rowny czasowi t
+			wektorG.push(wektorN.top()); ///do wektora G dodajemy pierwszy element z wektora N
+			wektorN.pop(); ///usuwamy pierwszy element z wektora N
+		}
+		/// dla pustego wektora G - brak zadan do uszeregowania - aktualizujemy pomocniczy czas
+		/// jesli wektor G nie jest pusty to aktualizujemy 't' o czas przygotowania
+		if (wektorG.empty() == true)
+			t = wektorN.top().r;
+		else {
+			RPQ kolejnosc;
+			kolejnosc = wektorG.top();
+			cout << kolejnosc;
+			wektorG.pop();
+			k += 1;
+			t += kolejnosc.p;
+			Cmax = max(Cmax, t + kolejnosc.q);
+		}
+	}
+
+	cout << "\n\nNasz Cmax wynosi: " << Cmax << endl;
+	_getch();
+}
